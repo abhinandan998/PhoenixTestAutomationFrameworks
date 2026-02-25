461d3903c2f43e3c7b6e20c7c695ae2039e22f37
abhinandan998 <basuabhinandan99@gmail.com>
2026-02-25 11:52:44 +0530

    Initial Commit

--- src/main/java/org/Abhinandan_Project/App.java
+++ src/main/java/org/Abhinandan_Project/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.Abhinandan_Project;
 
 /**
  * Hello world!
--- src/test/java/org/Abhinandan_Project/API/Tests/LoginAPITest.java
+++ src/test/java/org/Abhinandan_Project/API/Tests/LoginAPITest.java
@@ -1,4 +1,42 @@
 package org.Abhinandan_Project.API.Tests;
 
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.Abhinandan_Project.API.POJOS.UserCredentials;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.*;
+
+import static io.restassured.RestAssured.*;
+
 public class LoginAPITest {
+
+    @Test
+    public void LoginAPITest(){
+
+        UserCredentials userCredentials = new UserCredentials("iamfd", "password");
+        given().
+                baseUri("http://64.227.160.186:9000/v1")
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .accept(ContentType.JSON)
+                .and()
+                .body(userCredentials)
+                .log().all()
+
+                .when()
+                .post("login")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .time(lessThan(2000L))
+                .and()
+                .body("message", equalTo("Success"))
+                .and()
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response_schema/LoginResponseSchema.json"));
+
+
+
+    }
 }
